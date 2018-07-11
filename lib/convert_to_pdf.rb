8b2062ecd7485f5ea09690006b1d6ac3c196ee77
Jordan Palmer <jordan.s.palmer@outlook.com>
2018-07-11 11:50:28 -0700

    Make sure generated PDF spans specified dimensions
    While the document size was correct, the PDF being generated was smaller than anticipated.

--- lib/convert_to_pdf.rb
+++ lib/convert_to_pdf.rb
@@ -8,29 +8,21 @@ module ConvertToPdf
     require 'chunky_png'
     require 'pdfkit'
 
-    barcode = Barby::Code128.new("99999")
-    #barocde2 = Barby::Code39.new("12345")
-    #puts "barcode"
-    #puts barcode
-
-    #Barby::PngOutputter.new(546456)
+    barcode = Barby::Code128.new(id)
 
     png = Barby::PngOutputter.new(barcode).to_png
 
-    #IO.write(Rails.root.join("lib/temp_qr.png"), png, 'wb')
-
-    File.open(Rails.root.join("lib/temp_qr.png"), 'wb') do |file|
+    File.open(Rails.root.join("lib/temp_barcode.png"), 'wb') do |file|
       file.write(png)
     end
-    
-    html = File.open(Rails.root.join("lib/template.html")).readlines.join("\n")
-    html = html.gsub("***", name)
-    #html = html.gsub("@@@", html)
 
-    kit = PDFKit.new(html, :page_height => 76.2, :page_width => 101.6, :margin_top => 0, :margin_bottom => 0, :margin_left => 0, :margin_right => 0)
+    html = ApplicationController.render(file: Rails.root.join('lib/template.html.erb'), assigns: {path: Rails.root.join("lib") , name: name })
 
-    #file = kit.to_file(Rails.root.join("lib/templace.html")
+    kit = PDFKit.new(html, :page_height => 76.2, :page_width => 101.6, :margin_top => 0, :margin_bottom => 0, :margin_left => 0, :margin_right => 0,
+     dpi: 400)
+
     kit.to_pdf
+
   end
 
 end
