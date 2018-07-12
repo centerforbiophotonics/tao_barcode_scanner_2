d61d2537dbf5d6559c756cd15090ad2032a2f692
Jordan Palmer <jordan.s.palmer@outlook.com>
2018-07-12 16:47:41 -0700

    Instead of generating barcode image file, hold base64 encoding of image directly in memory without writing to disk
    Context: We can still render the image in the relevant view using the encoding that is passed into memory, while avoiding disk writes as a result of this.

--- lib/convert_to_pdf.rb
+++ lib/convert_to_pdf.rb
@@ -9,17 +9,12 @@ module ConvertToPdf
 
     barcode = Barby::Code128.new(id)
 
-    png = Barby::PngOutputter.new(barcode).to_png
+    png = Barby::PngOutputter.new(barcode).to_image.to_data_url
 
-    File.open(Rails.root.join("lib/temp_barcode.png"), 'wb') do |file|
-      file.write(png)
-    end
-
-    html = ApplicationController.render(file: Rails.root.join('app/views/tao/template.html.erb'), assigns: {path: Rails.root.join("lib") , name: name })
+    html = ApplicationController.render(file: Rails.root.join('app/views/tao/template.html.erb'), assigns: {path: Rails.root.join("lib") , name: name, png: png })
 
     kit = PDFKit.new(html, :page_height => 76.2, :page_width => 101.6, :margin_top => 0, :margin_bottom => 0, :margin_left => 0, :margin_right => 0,
      dpi: 400)
-    kit = PDFKit.new('http://google.com')
 
     kit.to_pdf
 
