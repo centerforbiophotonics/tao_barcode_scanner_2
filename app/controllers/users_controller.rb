7e4f9df9f4be0893810d0dd9ef0bed06291ce5bc
Jordan Palmer <jordan.s.palmer@outlook.com>
2018-07-31 16:00:22 -0700

    Add add, update, and delete methods to users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,41 @@
 class UsersController < ApplicationController
+	skip_before_action :verify_authenticity_token, :only => [:add, :delete, :update]
 
 	def user
-		puts "hello"
 		@users = User.all
 	end
 
-	def add_user
+	def add
+		u = User.new
+		u.name = params[:name]
+		u.email = params[:email]
+		u.cas_user = params[:id]
+		u.save!
+
+		render :json => User.all
 	end
 
+	def update
+		if User.exists?(:id => params[:id])
+			u = User.find_by(:id => params[:id])
+			if !params[:name].blank? 
+				u.name = params[:name]
+			end
+			if !params[:email].blank? 
+				u.email = params[:email]
+			end
+			u.save!
+			puts "User #{u.name}, ID #{u.cas_user} updated."
+		end
+		render :json => User.all
+	end
+
+	def delete
+		if User.exists?(:id => params[:id])
+			u = User.find_by(:id => params[:id])
+			puts "User #{u.name}, ID #{u.cas_user} destroyed."
+			u.destroy
+		end
+		render :json => User.all
+	end
 end
