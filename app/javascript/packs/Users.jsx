8061dfcbbc7c74823aadfd7cb6878f0ae0ce072d
Jordan Palmer <jordan.s.palmer@outlook.com>
2018-07-31 16:04:14 -0700

    Update Users.jsx to dynamically display the appropriate forms
    Also includes new methods for handling differing results received from the AJAX calls in User_form.jsx

--- app/javascript/packs/Users.jsx
+++ app/javascript/packs/Users.jsx
@@ -17,15 +17,67 @@ class Users extends Component {
 	constructor(props) {
 		super(props);
     this.showAddUser = this.showAddUser.bind(this)
-		this.state = {showAddUser: false};
+    this.handleAddResults = this.handleAddResults.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+    this.handleEdit = this.handleEdit.bind(this)
+    this.handleEditCancel = this.handleEditCancel.bind(this)
+		this.state = {showEdit: false,
+                  showAddUser: false,
+                  selectedUser: {},
+                  users: []}
 	}
 
   showAddUser(attendee_id) {
    this.state.showAddUser ? this.setState({showAddUser: false}) : this.setState({showAddUser: true});
   }
 
+  handleAddResults(data) {
+    this.setState({users: data});
+    if (this.state.showEdit) {
+      this.setState({showEdit: false});
+    } 
+  }
+
+  handleEditCancel(data) {
+    this.setState({users: data});
+    if (this.state.showEdit) {
+      this.setState({showEdit: false});
+    } 
+  }
+
+  handleDelete(id) {
+    console.log("submit");
+    fetch(this.props.url + "users/delete", {
+        method: 'post',
+        body: JSON.stringify({id: id}), //send string ID instead of numerical ID for attendee_id
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        credentials: 'include'
+      })
+      .then(response => response.json())
+      .then(data => this.setState({users: data}))
+  }
+
+  handleEdit(user) {
+    console.log("edit");
+    this.setState({showEdit:"true"});
+    this.setState({selectedUser: user});
+  }
+
+  handleEditCancel() {
+    if (this.state.showEdit) {
+      this.setState({showEdit: false});
+    } 
+  }
+
+
 	render() {
-    console.log(this.props);
+    //console.log(this.props.users);
+    this.props.users.forEach((u) => console.log(u));
+    console.log(this.props.users && this.props.users.length);
+    this.props.users.map((u) => console.log(u));
+    console.log(this.state.users);
 		return(
       <Grid>
         <Row>
@@ -43,17 +95,15 @@ class Users extends Component {
         <Row>
           <p>Current users:</p>
         </Row>
+        {(this.state.users && this.state.users.length) ? this.state.users.map((u) => <Row style={{paddingBottom: "5px"}}><Col md={3}>{u.name}</Col> <Col md={3}>{u.email}</Col> <Col md={3}><Button bsStyle="warning" bsSize="small" onClick={() => this.handleEdit(u)}>Update</Button>&nbsp;<Button bsStyle="danger" bsSize="small" onClick={() => this.handleDelete(u.id)}>Delete</Button> </Col></Row>) : <p>There are currently no registered users.</p>}
         <Row>
-        {(this.props.users && this.props.users.length) ? this.props.users.forEach((u) => <p>{u.name}</p>) : <p>There are currently no registered users.</p>}
-        </Row>
-        <Row>
-          <Button bsStyle="primary" onClick={this.showAddUser}>
+          <Button bsStyle="primary" onClick={this.showAddUser} disabled={this.state.showEdit ? true : false}>
             Add new user
           </Button>
         </Row>
         <Row>
-        {this.state.showAddUser ?
-          <AddUserForm />
+        {this.state.showAddUser || this.state.showEdit ?
+          <AddUserForm defaultProps={this.props} handleResultsResponse={this.handleAddResults} handleEditCancel={this.handleEditCancel} showEdit={this.state.showEdit} selectedUser={this.state.selectedUser}/>
           :
           null
         }
@@ -61,6 +111,10 @@ class Users extends Component {
       </Grid>
       );
 	}
+
+  componentDidMount(){
+    this.setState({users: this.props.users});
+  }
 }
 
 export default Users;
